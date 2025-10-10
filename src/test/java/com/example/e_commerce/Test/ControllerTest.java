6620b202e34ef51fd22b426dd51df9db3b38f469
phanindra-Dev <phanindrajaychand56@gmail.com>
2025-10-10 09:44:57 +0530

    Junit Testing

--- src/test/java/com/example/e_commerce/Test/ControllerTest.java
+++ src/test/java/com/example/e_commerce/Test/ControllerTest.java
@@ -1,5 +1,20 @@
 package com.example.e_commerce.Test;
 
+import com.example.e_commerce.Entries.User;
+import com.example.e_commerce.repo.UserRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class ControllerTest {
 
 }
